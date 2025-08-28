a443346e2483f3109eab9f778bddc940bd4923c8
mjnewman33 <118640619+mjnewman33@users.noreply.github.com>
2025-08-28 19:36:19 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hawaii-trip-v8';  // bumped version
+const CACHE_NAME = 'hawaii-trip-v9';  // bumped version
 const urlsToCache = [
   '/',
   '/index.html'
@@ -45,6 +45,7 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
 
 
 
