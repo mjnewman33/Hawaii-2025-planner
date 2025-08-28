70adad24b8cd8e772114487765e6e839518d585f
mjnewman33 <118640619+mjnewman33@users.noreply.github.com>
2025-08-28 16:18:46 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hawaii-trip-v2';
+const CACHE_NAME = 'hawaii-trip-v3';
 const urlsToCache = [
   '/',
   '/index.html'
@@ -37,4 +37,5 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
