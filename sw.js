87496685db0822387e2149fce70c96e02d16c008
mjnewman33 <118640619+mjnewman33@users.noreply.github.com>
2025-08-28 16:16:49 -0400

    Update sw.js
    updated to delete html cache

--- sw.js
+++ sw.js
@@ -13,8 +13,17 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
+
 
 self.addEventListener('fetch', event => {
   if (event.request.url.includes('script.google.com')) {
@@ -28,3 +37,4 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
